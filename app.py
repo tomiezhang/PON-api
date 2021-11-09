8d4d1395325a9f765a711ee5bb602f2bea2b7ebe
tomiezhang <tomieweb@gmail.com>
2021-11-10 02:22:14 +0800

    v1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 import json
 from PIL import Image
 import requests
+import os
 import random
 '''
 #部署在自己服务器上的metadta-api服务器
@@ -26,40 +27,54 @@ def hello_world():  # put application's code here
 @app.route('/api/opera/<token_id>')
 def opera(token_id):
     token_id = int(token_id)
-    if token_id < TOTAL:
-        background = BACKGROUND[token_id % len(BACKGROUND)]
-        ear = EAR[token_id % len(EAR)]
-        eye = EYE[token_id % len(EYE)]
-        face = FACE[token_id % len(FACE)]
-        mouth = MOUTH[token_id % len(MOUTH)]
-        nose = NOSE[token_id % len(NOSE)]
-        print("{}-{}-{}-{}-{}-{}".format(background,ear,eye,face,mouth,nose))
-        image_url = _compose_image(['static/background/{}.jpg'.format(background),'static/ear/{}.png'.format(ear),'static/face/{}.png'.format(face),'static/mouth/{}.png'.format(mouth),'static/nose/{}.png'.format(nose),'static/eye/{}.png'.format(eye)],token_id)
-        attributes = []
-        _add_attribute(attributes, 'background', BACKGROUND, token_id)
-        _add_attribute(attributes, 'ear', EAR, token_id)
-        _add_attribute(attributes, 'eye', EYE, token_id)
-        _add_attribute(attributes, 'face', FACE, token_id)
-        _add_attribute(attributes, 'mouth', MOUTH, token_id)
-        _add_attribute(attributes, 'nose', NOSE, token_id)
-        # 级别游戏可用_add_attribute(attributes, 'level', INT_ATTRIBUTES, token_id)
-        #耐力游戏可用_add_attribute(attributes, 'stamina', FLOAT_ATTRIBUTES, token_id)
-        #性格游戏可用_add_attribute(attributes, 'personality', STR_ATTRIBUTES, token_id)
-        #水上力量游戏可用_add_attribute(attributes, 'aqua_power', BOOST_ATTRIBUTES, token_id, display_type="boost_number")
-        #耐力增长游戏可用_add_attribute(attributes, 'stamina_increase', PERCENT_BOOST_ATTRIBUTES, token_id, display_type="boost_percentage")
-        #代数游戏可用_add_attribute(attributes, 'generation', NUMBER_ATTRIBUTES, token_id, display_type="number")
-        if image_url !=1:
-            return jsonify({
-                'name': "CryptoOpera %s" % token_id,
-                'description': "Crpto feelings from the far east.",
-                'image': image_url,
-                #'external_url': 'https://example.com/?token_id=%s' % token_id,
-                'attributes': attributes
-            })
+    if token_id<TOTAL:
+        # 查找output里是否生成过，生成过就不再生成直接返回元数据
+        if os.path.exists("static/output/" + str(token_id) + ".json"):
+            with open("static/output/" + str(token_id) + ".json", 'r') as load_f:
+                load_dict = json.load(load_f)
+                return jsonify(load_dict)
         else:
-            return jsonify({
-                'stats': "wow!404"
-            })
+            #没有生成过
+            background = BACKGROUND[token_id % len(BACKGROUND)]
+            ear = EAR[token_id % len(EAR)]
+            eye = EYE[token_id % len(EYE)]
+            face = FACE[token_id % len(FACE)]
+            mouth = MOUTH[token_id % len(MOUTH)]
+            nose = NOSE[token_id % len(NOSE)]
+            print("{}-{}-{}-{}-{}-{}".format(background, ear, eye, face, mouth, nose))
+            image_url = _compose_image(['static/background/{}.jpg'.format(background), 'static/ear/{}.png'.format(ear),
+                                        'static/face/{}.png'.format(face), 'static/mouth/{}.png'.format(mouth),
+                                        'static/nose/{}.png'.format(nose), 'static/eye/{}.png'.format(eye)], token_id)
+            attributes = []
+            _add_attribute(attributes, 'background', BACKGROUND, token_id)
+            _add_attribute(attributes, 'ear', EAR, token_id)
+            _add_attribute(attributes, 'eye', EYE, token_id)
+            _add_attribute(attributes, 'face', FACE, token_id)
+            _add_attribute(attributes, 'mouth', MOUTH, token_id)
+            _add_attribute(attributes, 'nose', NOSE, token_id)
+            # 级别游戏可用_add_attribute(attributes, 'level', INT_ATTRIBUTES, token_id)
+            # 耐力游戏可用_add_attribute(attributes, 'stamina', FLOAT_ATTRIBUTES, token_id)
+            # 性格游戏可用_add_attribute(attributes, 'personality', STR_ATTRIBUTES, token_id)
+            # 水上力量游戏可用_add_attribute(attributes, 'aqua_power', BOOST_ATTRIBUTES, token_id, display_type="boost_number")
+            # 耐力增长游戏可用_add_attribute(attributes, 'stamina_increase', PERCENT_BOOST_ATTRIBUTES, token_id, display_type="boost_percentage")
+            # 代数游戏可用_add_attribute(attributes, 'generation', NUMBER_ATTRIBUTES, token_id, display_type="number")
+            if image_url != 1:
+                #如果上传IPFS没有问题
+                jsons = {
+                    'name': "CryptoOpera %s" % token_id,
+                    'description': "Crpto feelings from the far east.",
+                    'image': image_url,
+                    # 'external_url': 'https://example.com/?token_id=%s' % token_id,
+                    'attributes': attributes
+                }
+                # 写入本地服务器
+                with open("static/output/" + str(token_id) + ".json", "w") as f:
+                    json.dump(jsons, f)
+                return jsonify(jsons)
+            else:
+                return jsonify({
+                    'stats': "wow!404"
+                })
     else:
         return jsonify({
             'stats': "wow!404"
